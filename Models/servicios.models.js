9f2de2d58bc2ccf243880b70ae1cc7ba16e11afc
EugeneWu1 <eugene.wu@unah.hn>
2025-07-25 13:35:54 -0600

    Se agrega parte de modelos para los servicios

---
+++ Models/servicios.models.js
@@ -0,0 +1,19 @@
+import pool from '../database/db.js';
+
+//getAllServicios,insertServicio,updateServicio,deleteServicio
+
+export const getAllServicios = async() => {
+
+}
+
+export const insertServicio = async() => {
+
+}
+
+export const updateServicio = async() => {
+
+}
+
+export const deleteServicio = async() => {
+
+}
