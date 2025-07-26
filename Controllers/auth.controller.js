b4fa1c5c2d5102309dcb9c75ddfb6d5151ebe20d
ErickRonaldo7 <emendeza@unah.hn>
2025-07-26 00:06:35 -0600

    RESEND-TEMPLATE

--- Controllers/auth.controller.js
+++ Controllers/auth.controller.js
@@ -3,6 +3,8 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import {v4 as uuidv4} from 'uuid';
 import {register}from '../Models/auth.models.js';
+import {Resend} from 'resend';
+import { createAccountEmailHTML } from '../emailTemplate.js';
 
 
 
@@ -21,10 +23,21 @@ export const createClient = async (req, res) => {
     const id = uuidv4()
 
     //generar un hash de contraseña temporal
-    const password_hash = await bcrypt.hash('1234', 10)
+    const tempPassword = '1234@newClient'; 
+    const password_hash = await bcrypt.hash(tempPassword, 10)
 
     try {
         const result = await register([id, name, email, phone, role, password_hash])
+
+        //enviar a correo 
+        const resend = new Resend(process.env.RESEND_API_KEY)
+
+        await resend.emails.send({
+            from: 'noreply@emewesdev.com',
+            to: email,
+            subject: 'Creación de cuenta',
+            html: createAccountEmailHTML(tempPassword) 
+        });
 
 
         res.json({
