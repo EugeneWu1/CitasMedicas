a8e46f9a4389fd2c8e3eb8f6df1eba11e02ed70a
EugeneWu1 <eugene.wu@unah.hn>
2025-07-26 00:09:21 -0600

    Se termina de programar el controlador

--- Controllers/servicios.controller.js
+++ Controllers/servicios.controller.js
@@ -1,27 +1,160 @@
-//import {validateServices} from '../Schemas/servicios.schema.js'
-//import {getAllServicios,getServiciosPorDisponibilidad,cambiarDisponibilidad,insertServicio,updateServicio,deleteServicio} '../models/servicios.models.js'
-//import {v4 as uuidv4} from uuid
+import {validateServices, validateServicesUpdate, validateAvailability} from '../Schemas/servicios.schema.js'
+import {getAllServicios,getServiciosPorDisponibilidad,cambiarDisponibilidad,insertServicio,updateServicio,deleteServicio} from '../Models/servicios.models.js'
+import {v4 as uuidv4} from uuid
 
-export const getAll = (req,res) => {
+export const getAll = async (req,res) => {
+    try {
+        const servicesDB = await getAllServicios()
 
+        res.status(200).json({
+            success: true,
+            data: servicesDB
+        })
+    } catch (error) {
+        res.status(400).json({
+            success:false,
+            message: 'Error al obtener servicios',
+            error: error.message
+        })
+    }
 }
 
-export const availableService = (req,res) => {
+export const availableService = async (req,res) => {
+    try {
+        const disponible = req.query.available === 'true'
 
+        const results = await getServiciosPorDisponibilidad(disponible)
+
+        res.status(200).json({
+            success: true,
+            data: results
+        })
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            message: 'Error al consultar servicios disponibles',
+            error: error.message
+        })
+    }
 }
 
-export const createService = (req,res) => {
+export const createService = async(req,res) => {
+    const data = req.body
 
+    const {success, error, data: safeData} = validateServices(data)
+
+    if(!success){
+        res.status(400).json({
+            success: false,
+            message: 'Error al crear servicio.',
+            error: error.message
+        })
+    }
+
+    //Codificar el id
+    const id = uuidv4()
+    safeData.id = id
+
+   
+    try {
+        const response = await insertServicio(safeData)
+
+        res.status(201).json({
+            success: true,
+            data: response
+        })
+
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            message: 'Error al insertar servicio.',
+            error: error.message
+        })
+    }
 }
 
-export const updateService = (req,res) => {
+export const updateService = async (req,res) => {
+    const {id} = req.params
 
+    const dataValidada = validateServicesUpdate(req.body)
+
+    if(!dataValidada.success){
+        return res.status(400).json({
+            success: false,
+            message: 'Datos invalidos.',
+            error: error.message
+        })
+    }
+
+    const dataFiltrada = dataValidada.data
+
+    try {
+        const result = await updateServicio(id,dataFiltrada)
+        res.json(result)
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: 'Error al actualizar servicio.',
+            error: error.message
+        })
+    }
 }
 
-export const changeAvailability = (req,res) => {
+export const changeAvailability = async (req,res) => {
+    const {id} = req.params
 
+    const validacion = validateAvailability(req.body)
+
+    if(!validacion.success){
+        return res.status(400).json({
+        success: false,
+        message: 'Datos inválidos para disponibilidad',
+        errors: validacion.error.errors
+        })
+    }
+
+    const {disponible} = validacion.data
+
+    try {
+    const result = await cambiarDisponibilidad(id, disponible)
+    res.json({
+      success: true,
+      message: 'Disponibilidad actualizada correctamente',
+      data: result
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Error al actualizar disponibilidad',
+      error: error.message
+    })
+  }
 }
 
-export const deleteService = (req,res) => {
+export const deleteService = async (req,res) => {
+    const {id} = req.params
 
+    res.json({
+      success: true,
+      message: 'Servicio eliminado correctamente',
+      data: result
+    });
+
+    try {
+        const result = await deleteServicio(id)
+
+        res.json({
+            success: true,
+            message: 'Servicio eliminado correctamente',
+            data: result
+        })
+        
+    } catch (error) {
+        res.status(500).json({
+        success: false,
+        message: 'Error al eliminar servicio',
+        error: error.message
+        });
+    }
 } 
--- Schemas/servicios.schema.js
+++ Schemas/servicios.schema.js
@@ -1,5 +1,31 @@
 import zod from 'zod'
 
+//Schema general
 const serviceSchema = zod.Object({
-    
-})
+  "nombre": zod.string().min(5).max(50),
+  "descripcion": zod.string().min(10).max(200),
+  "duracion": zod.number(),
+  "precio": zod.number(),
+  "disponible": zod.boolean()
+}).strict()
+
+//Schema para validar cambios de estado
+const availabilitySchema = zod.Object({
+  "disponible":zod.boolean()
+}).strict()
+
+
+//Para el update hay que habilitar campos a opcionales
+const serviceUpdateSchema = serviceSchema.partial().strict()
+
+export const validateServices = (services) => {
+    return serviceSchema.safeParse(services)
+}
+
+export const validateServiceUpdate = (data) => {
+  return serviceUpdateSchema.safeParse(data)
+}
+
+export const validateAvailability = (data) => {
+  return availabilitySchema.safeParse(data)
+}
